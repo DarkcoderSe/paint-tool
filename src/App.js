61a2a8755ee889906c6f4205aa35dd8653825692
DarkcoderSe <kashifs294@gmail.com>
2020-05-19 14:03:10 +0500

    B

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+
+    this.boardIntializer();
+
+  }
+
+  boardIntializer = () => {
+    console.log('Board Intialized');
+    const canvasContainer = $('#boardDiv');
+
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('width', 600);
+    canvas.setAttribute('height', 400);
+    canvas.setAttribute('id', 'canvas');
+
+    canvasContainer.appendChild(canvas);
+
+    // if (typeof G_vmlCanvasManager != 'undefined') {
+    //   canvas = G_vmlCanvasManager.initElement(canvas);
+    // }
+    const context = canvas.getContext('2d');
+  }
+
   render() {
     return (
       <div className="board-container">
