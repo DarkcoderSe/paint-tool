42c1dac90ff4e7c49087a9b26f32aadd5e6dda95
DarkcoderSe <kashifs294@gmail.com>
2020-05-19 13:47:43 +0500

    boar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="board-container">
         <div id="boardDiv"></div>
 
         <button id="clearBoardBtn">
